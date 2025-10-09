90d58fda1ca42262793ba5afc5c72af1df41e46c
Jonathan Lennox <jonathan.lennox@8x8.com>
2025-10-09 15:33:12 -0400

    Remove another unused parameter.

--- src/utils.ts
+++ src/utils.ts
@@ -2,19 +2,16 @@ export interface ISessionParameters {
     url: URL,
     sessionId: string | null;
     transcribe: boolean;
-    transcriberType: string;
 }
 
 export function extractSessionParameters(url: string): ISessionParameters {
     const parsedUrl = new URL(url);
     const sessionId = parsedUrl.searchParams.get("sessionId");
-    const type = parsedUrl.searchParams.get("type") ?? "openai";
     const transcribe = parsedUrl.pathname.endsWith("/transcribe");
 
     return {
         url: parsedUrl,
         sessionId,
-        transcribe,
-        transcriberType: type
+        transcribe
     };
 }
